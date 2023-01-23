83811bcff1765189532e3da2e655234c2144bfa9
humaiun <humaiunkobirr@gmail.com>
2023-01-24 02:54:42 +0600

    added terms & conditions

--- src/pages/TermsConditions/TermsConditions.js
+++ src/pages/TermsConditions/TermsConditions.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const TermsConditions = () => {
   return (
-    <section className="container mt-5 pt-5">
+    <section className="container my-5 pt-5">
       <h1 className="fw-light">Terms & Conditions</h1>
       <div className="mt-4">
         <div>
@@ -25,10 +25,79 @@ const TermsConditions = () => {
         </div>
         <div>
           <i className=" fw-bold">Image Copyright Policy:</i>
-
           <p className="li-sm mb-2">
             Clippers Web accepts payments via PayPal, MasterCard, Visa card, bank transfers & all major
             credits cards supported by PayPal.
+          </p>
+        </div>
+        <div>
+          <i className=" fw-bold">Refund Policy:</i>
+          <p className="li-sm mb-2">
+            If a client is not happy with the service, he/she can claim his/her money back within 2 weeks. We
+            will transfer the whole amount to the client’s bank account or any other payment option preferred
+            by the client. Clippers Web will not be responsible for anything after 2 weeks of any service has
+            been delivered. For refund please email us at info@clippersweb.com.
+          </p>
+        </div>
+        <div>
+          <i className=" fw-bold">Quality Guarantee:</i>
+          <p className="li-sm mb-2">
+            Clippers Web strives to provide services of the very highest quality. In the unlikely event that
+            you are not satisfied with the results, we will make reasonable corrections and amendment as
+            necessary without additional charge. CW reserves the right not to make amendments if the error is
+            the result of poor communication on the part of the Client. Any amendments made as part of our
+            quality guarantee are without prejudice to CW.
+          </p>
+        </div>
+        <div>
+          <i className=" fw-bold">File Storage Policy:</i>
+          <p className="li-sm mb-2">
+            We keep backups of the old done/raw files for two months. After two months our backup server
+            automatically deletes the old files. If a customer needs his image files, he must notify us within
+            these 2 months. Only then we can re-upload those image files.
+          </p>
+        </div>
+        <div>
+          <i className=" fw-bold">Email Policy:</i>
+          <p className="li-sm mb-2">
+            Clippers Web will not sell or reveal your emails to anyone third-party unless authorized by the
+            visitor. In case of advertising to specific email list, we may use your email and other contact
+            information. But, most of the places we host your emails and contact information for Online
+            Advertisement are safe and secured.
+          </p>
+          <p className="li-sm mb-2">
+            CW does not send “junk” e-mail, also known as “spam”. In certain instances, when CW wishes to
+            contact customers via e-mail, respond to messages, confirm purchases, or transmit information
+            requested by a visitor, CW will use e-mail.
+          </p>
+          <p className="li-sm mb-2">
+            When a customer or visitor receives an e-mail which s\he does not want from CW, the customer
+            always can opt-out from getting further e-mails. Please contact us at info@clippersweb.com for
+            this.
+          </p>
+        </div>
+        <div>
+          <i className=" fw-bold">Cookies Policy:</i>
+          <p className="li-sm mb-2">
+            Clippers Web (CW) values the privacy of its customers and business partners and is dedicated to
+            abiding by all laws pertaining to the protection of personal data. We use cookies to provide
+            better user experience for web elements and resources. Without cookies, some functions of the site
+            may not work properly, e.g. showing custom banners, offers, and videos.
+          </p>
+          <p className="li-sm mb-2">
+            Cookies are small files sent from our server to your browser. Cookies are transmitted back
+            unaltered by your browser each time our server is accessed. The data is used for accumulating user
+            selections and follow user trends and search footprints. Cookies do not identify you. CW does not
+            and cannot utilize cookies to recover particularly identifiable data except if willingly supplied
+            by our visitors. Neither can we acquire data from your hard drive or transmit computer virus.
+            Cookies also maintain the visitor’s identity while at our web site based on your online behavior.
+            Most browsers opt-in to cookies automatically, but you can alter the settings of your browser to
+            prevent automatic opt-in.
+          </p>
+          <p className="li-sm mb-2">
+            CW also uses cookies to deliver advertising messages and custom offers on other relevant websites.
+            Since, we do not spam your e-mail, it helps us to reach you with our custom messages and offers
+            through online advertisement. Clients can opt out of our offers and decline cookies at any time.
           </p>
         </div>
       </div>
